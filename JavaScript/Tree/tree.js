8ea4d1e3d82d9c950114779d3403fde3e0bb0947
fn <xuexing9527@163.com>
2024-03-18 23:27:45 +0800

    feat: create tree undone

--- JavaScript/Tree/tree.js
+++ JavaScript/Tree/tree.js
@@ -120,4 +120,76 @@ const getTree = () => JSON.parse(JSON.stringify({
     ]
 }))
 
+// 随机生成包含多少节点的树
+class Node {
+    constructor({ name, key }) {
+        this.name = name
+        this.key = key
+    }
+}
+
+// 
+const tree = (nums) => {
+    const arr = []
+    for (let i = 1; i < nums; i += 1) {
+        arr.push(new Node({ name: `节点${i}`, key: `${i}` }))
+    }
+
+    const toTree = (nodesArray) => {
+        // 选一个根节点
+        const rootNode = nodesArray.pop()
+        console.log(rootNode)
+
+        const toArray = (tree) => {
+            const arr = []
+            if (tree.children) {
+                return tree.children.reduce((pre, current) => {
+                    console.log(pre)
+                    console.log(current)
+                    if (current.children) {
+                        arr.concat(current.children)
+                        current.children.map(item => toArray(item))
+                    } else {
+                        arr.push(current)
+                    }
+                }, arr)
+            }
+
+            arr.push(tree)
+            return arr
+        }
+
+        const getLuckyIndex = arr => parseInt(Math.random() * arr.length)
+        const getLuckyChildrenLen = arr => parseInt(Math.random() * arr.length)
+
+        const recurson = (tree, array) => {
+            if (!array.length) return
+
+            const getLuckyNode = (tree) => {
+                const havebeenTreeNodeArray = toArray(tree)
+                console.log(1, havebeenTreeNodeArray)
+                console.log(getLuckyIndex(havebeenTreeNodeArray))
+                return havebeenTreeNodeArray[getLuckyIndex(havebeenTreeNodeArray)]
+            }
+
+            const luckyNode = getLuckyNode(tree)
+            console.log(luckyNode)
+            const luckyChildrenLen = getLuckyChildrenLen(array)
+            console.log(luckyChildrenLen)
+
+            luckyNode.children = luckyNode.children || []
+            new Array(luckyChildrenLen).fill(0).map(_ => {
+                luckyNode.children.push(array[getLuckyIndex(array)])
+            })
+            recurson(tree, array)
+        }
+
+        recurson(rootNode, nodesArray)
+    }
+
+    toTree(arr)
+}
+
+tree(10)
+
 module.exports = getTree
