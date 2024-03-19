38a6fa6d8ff0c17fc9fe97a925ee93abbdd10d1e
fn <xuexing9527@163.com>
2024-03-19 11:03:13 +0800

    feat: createTree done

--- JavaScript/Tree/tree.js
+++ JavaScript/Tree/tree.js
@@ -1,125 +1,3 @@
-const getTree = () => JSON.parse(JSON.stringify({
-    key: '1',
-    pKey: '0',
-    children: [
-        {
-            key: '1_1',
-            pKey: '1',
-            children: [
-                {
-                    key: '1_1_1',
-                    pKey: '1_1',
-                    children: [
-                        {
-                            key: '1_1_1_1',
-                            pKey: '1_1_1',
-                            children: [
-                                {
-                                    key: '1_1_1_1_1',
-                                    pKey: '1_1_1_1'
-                                }
-                            ]
-                        },
-                        {
-                            key: '1_1_1_2',
-                            pKey: '1_1_1_1'
-                        },
-                        {
-                            key: '1_1_1_3',
-                            pKey: '1_1_1_1'
-                        },
-                    ]
-                },
-                {
-                    key: '1_1_2',
-                    pKey: '1_1',
-                },
-                {
-                    key: '1_1_3',
-                    pKey: '1_1',
-                },
-            ]
-        },
-        {
-            key: '1_2',
-            pKey: '1',
-            children: [
-                {
-                    key: '1_2_1',
-                    pKey: '1_2',
-                    children: [
-                        {
-                            key: '1_2_1_1',
-                            pKey: '1_2_1',
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            key: '1_3',
-            pKey: '1',
-            children: [
-                {
-                    key: '1_3_1',
-                    pKey: '1_3',
-                    children: [
-                        {
-                            key: '1_3_1_1',
-                            pKey: '1_3',
-                            children: [
-                                {
-                                    key: '1_3_1_1_1',
-                                    pKey: '1_3_1_1'
-                                }
-                            ]
-                        },
-                        {
-                            key: '1_3_1_2',
-                            pKey: '1_3_1'
-                        },
-                        {
-                            key: '1_3_1_3',
-                            pKey: '1_3_1'
-                        },
-                    ]
-                },
-                {
-                    key: '1_3_2',
-                    pKey: '1_3',
-                },
-                {
-                    key: '1_3_3',
-                    pKey: '1_3',
-                },
-
-            ]
-        },
-        {
-            key: '1_4',
-            pKey: '1',
-            children: [
-                {
-                    key: '1_4_1',
-                    pKey: '1_4',
-                },
-                {
-                    key: '1_4_2',
-                    pKey: '1_4',
-                    children: [
-                        {
-                            key: '1_4_2_1',
-                            pKey: '1_4_2',
-                        },
-                    ]
-                },
-
-            ]
-        }
-
-    ]
-}))
-
 // 随机生成包含多少节点的树
 class Node {
     constructor({ name, key }) {
@@ -128,68 +6,69 @@ class Node {
     }
 }
 
-// 
-const tree = (nums) => {
-    const arr = []
-    for (let i = 1; i < nums; i += 1) {
-        arr.push(new Node({ name: `节点${i}`, key: `${i}` }))
-    }
+const createTree = (nums) => {
+    const arr = new Array(nums).fill(0).map((_, i) => new Node({ name: `节点${i + 1}`, key: `${i + 1}` }))
 
-    const toTree = (nodesArray) => {
-        // 选一个根节点
-        const rootNode = nodesArray.pop()
-        console.log(rootNode)
-
-        const toArray = (tree) => {
-            const arr = []
-            if (tree.children) {
-                return tree.children.reduce((pre, current) => {
-                    console.log(pre)
-                    console.log(current)
-                    if (current.children) {
-                        arr.concat(current.children)
-                        current.children.map(item => toArray(item))
-                    } else {
-                        arr.push(current)
-                    }
-                }, arr)
-            }
-
-            arr.push(tree)
+    const toArray = (tree) => {
+        const arr = []
+        arr.push(tree)
+        if (tree.children) {
+            const res = tree.children.reduce((pre, current) => {
+                if (current.children) {
+                    arr.concat(current.children)
+                    current.children.map(item => toArray(item))
+                } else {
+                    arr.push(current)
+                }
+            }, arr)
             return arr
         }
+        return arr
+    }
 
-        const getLuckyIndex = arr => parseInt(Math.random() * arr.length)
-        const getLuckyChildrenLen = arr => parseInt(Math.random() * arr.length)
+    const getLuckyIndex = (arr) => {
+        if (arr && arr.length) {
+            return parseInt(Math.random() * arr.length)
+        }
+        return 0
+    }
 
+    const getLuckyChildrenLen = (arr) => {
+        return getLuckyIndex(arr) + 1
+    }
+
+    const getLuckyNode = (tree) => {
+        const havebeenTreeNodeArray = toArray(tree)
+        const luckyIndex = getLuckyIndex(havebeenTreeNodeArray)
+        return havebeenTreeNodeArray[luckyIndex]
+    }
+
+    const toTree = (rootNode, nodesArray) => {
+        // 消耗 array
         const recurson = (tree, array) => {
             if (!array.length) return
-
-            const getLuckyNode = (tree) => {
-                const havebeenTreeNodeArray = toArray(tree)
-                console.log(1, havebeenTreeNodeArray)
-                console.log(getLuckyIndex(havebeenTreeNodeArray))
-                return havebeenTreeNodeArray[getLuckyIndex(havebeenTreeNodeArray)]
-            }
-
             const luckyNode = getLuckyNode(tree)
-            console.log(luckyNode)
             const luckyChildrenLen = getLuckyChildrenLen(array)
-            console.log(luckyChildrenLen)
-
             luckyNode.children = luckyNode.children || []
             new Array(luckyChildrenLen).fill(0).map(_ => {
-                luckyNode.children.push(array[getLuckyIndex(array)])
+                luckyNode.children.push(array.splice(getLuckyIndex(array), 1)[0])
             })
+
             recurson(tree, array)
         }
 
         recurson(rootNode, nodesArray)
     }
 
-    toTree(arr)
+    // 选一个根节点
+    const rootNode = arr.shift()
+    toTree(rootNode, arr)
+
+    return rootNode
 }
 
-tree(10)
+// // test
+// const tree = createTree(20)
+// console.log(JSON.stringify(tree, null, 2))
 
-module.exports = getTree
+module.exports = createTree
