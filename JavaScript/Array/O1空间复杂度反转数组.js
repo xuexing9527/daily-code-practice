3d6f342fb96d29e2327ce314f6e434a74af247bc
xue xing <xuexing9527@163.com>
2024-03-22 17:07:52 +0800

    refactor: reversal arr

--- JavaScript/Array/O1空间复杂度反转数组.js
+++ JavaScript/Array/O1空间复杂度反转数组.js
@@ -3,12 +3,13 @@ const reArr = (arr) => {
     const len = arr.length
     const times = len / 2
     for (let i = 0; i < times; i += 1) {
-        // 引用关系
         const val = arr[len - (len - i)]
         arr[len - (len - i)] = arr[len - i - 1]
         arr[len - i - 1] = val
+        // 以上三行可以用下面一行解构代替
+        // [arr[len - (len - i)], arr[len - i - 1]] = [arr[len - i - 1], arr[len - (len - i)]]
     }
-    console.log(arr)
     return arr
 }
-reArr(arr)
+
+console.log(reArr(arr))
