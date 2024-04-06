16a1f751f2db31187239715ad22494aa8e8792ba
xue xing <xuexing9527@163.com>
2024-04-06 11:16:13 +0800

    feat: Rains something wrong. Let me check for a while ...

--- Bytedance/rain.js
+++ Bytedance/rain.js
@@ -28,9 +28,9 @@
 
 // 参考思考3实现，耗时 1h
 function getRain(arr) {
+    const time = new Date().getTime()
     const column = arr.length
     const row = arr.slice().sort()[column - 1]
-    console.log('column & row:', column, row)
     let rain = 0
 
     const handleArr = (arr) => {
@@ -67,30 +67,18 @@ function getRain(arr) {
             // 找首尾 0 
             tArr = handleArr(tArr)
         }
-        console.log('tArr', tArr)
+        // console.log('tArr', tArr)
 
-        tArr.map((item) => {
+        tArr.forEach((item) => {
             // 积累为 0 的雨滴
             if (item <= 0) {
                 rain += 1
             }
         })
     }
-    console.log('rains', rain)
+    console.log('getRain耗时：', new Date().getTime() - time)
     return rain
 }
-
-// const arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1] // 6 滴
-// const arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 0, 1] // 6 滴
-// const arr = [0, 1, 0, 2, 1, 0, 1, 0, 2, 1, 2, 1] // 8 滴
-// const arr = [0, 1, 0, 2, 1, 0, 1, 3, 0, 1, 2, 1] // 8 滴
-const arr = [0, 1, 0, 2, 0, 0, 1, 3, 2, 1, 2, 1] // 7 滴
-
-getRain(arr)
-
-
-
-
 
 // 思考4: 
 //      一边找首尾的不为0的值，一边计算雨滴，简化下代码
@@ -100,6 +88,7 @@ getRain(arr)
 // 
 
 function getRain2(arr) {
+    const time = new Date().getTime()
     const lines = arr.slice().sort()[arr.length - 1]
     const len = arr.length
     let rains = 0
@@ -108,7 +97,7 @@ function getRain2(arr) {
         const obj = { firstItem: undefined, lastItem: undefined }
         // 获取首尾
         for (let m = 0, n = len - 1; m <= n; m += 1, n -= 1) {
-            if (arr[m] - i >= 1 && obj.firstItem === undefined) obj.firstItem = m 
+            if (arr[m] - i >= 1 && obj.firstItem === undefined) obj.firstItem = m
             if (arr[n] - i >= 1 && obj.lastItem === undefined) obj.lastItem = n
         }
         // 同时存在
@@ -119,7 +108,74 @@ function getRain2(arr) {
                 .forEach(item => { if (item <= 0) rains += 1 })
         }
     }
+    console.log('getRain2耗时：', new Date().getTime() - time)
     return rains
 }
 
-console.log('rains', getRain2(arr))
+// 思考5: 
+//      分层积累，保留上一次计算结果，
+//      从开头找成立的点后，开始积累雨滴。如果当前层最终尾结点成立，则积累当前层雨滴，否则，舍弃当前层雨滴
+// 
+//         0
+//     0_ _00_0
+//  _0_00_000000 
+// 
+
+function getRain3(arr) {
+    const time = new Date().getTime()
+    // 由积累成功后的层级计算出下一层需要 遍历的 前后位置，存入数组 为了 和 i 建立关联关系
+    const levelStartAndEnd = [
+        { firstIndex: 0, lastIndex: arr.length } // 第一层 默认从头到尾遍历
+    ]
+    let i = 0
+    let rains = 0
+    while (true) {
+        let levelRains = 0
+        const obj = { firstIndex: undefined, lastIndex: undefined }
+        const preLevelObj = levelStartAndEnd[i]
+        if (preLevelObj) {
+            // 这里注意，奇数长度数组中间值 不会被遍历到。放到后面 m, n 存在的情况下，再判断中间值是否积累雨滴
+            for (let m = preLevelObj.firstIndex, n = preLevelObj.lastIndex; m < n; m += 1, n -= 1) {
+                if (arr[m] - i >= 1 && obj.firstIndex === undefined) { obj.firstIndex = m } // 从 m 位置开始积累雨滴
+                if (obj.firstIndex !== undefined && (m > obj.firstIndex && arr[m] - i <= 0)) { levelRains += 1 } // obj.firstIndex 存在时，arr[m] - i 小于等于 0 且 大于 m 位置时 积累雨滴
+                if (arr[n] - i >= 1 && obj.lastIndex === undefined) { obj.lastIndex = n } // 到 n 位置 结束积累雨滴，循环结束后，考虑 n 是否真的存在，如果不存在，当前层没有雨滴可接
+                if (obj.lastIndex !== undefined && (n < obj.lastIndex && arr[n] - i <= 0)) { levelRains += 1 } // obj.firstIndex 存在时，arr[m] - i 小于等于 0 且 大于 m 位置时 积累雨滴
+            }
+
+            // 如果 m, n 相遇，没有合适的 m 或 n，舍弃当前层的雨滴。只有当 m, n 都存在时，levelRains 才成立
+            if (obj.firstIndex !== undefined && obj.lastIndex !== undefined) {
+                levelStartAndEnd.push(obj) // 存入层级 obj，减少下次遍历的长度
+                const len = preLevelObj.lastIndex + 1 - obj.firstIndex
+                // 如果数组长度是奇数，判断中间节点是否满足接雨滴的条件
+                if (len % 2 !== 0) {
+                    const middleIndex = (preLevelObj.firstIndex + preLevelObj.lastIndex) / 2
+                    if (arr[middleIndex] - i <= 0) rains += 1
+                }
+                rains = rains + levelRains // 积累当前层雨滴
+            }
+        } else {
+            // 上一次都没积累雨滴，直接break
+            break;
+        }
+        i += 1
+    }
+    console.log('getRain3耗时：', new Date().getTime() - time)
+    return rains
+}
+
+
+
+// const arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1] // 6 滴
+// const arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 0, 1] // 6 滴
+// const arr = [0, 1, 0, 2, 1, 0, 1, 0, 2, 1, 2, 1] // 8 滴
+// const arr = [0, 1, 0, 2, 1, 0, 1, 3, 0, 1, 2, 1] // 8 滴
+const arr = [10000 ,0 ,999 , 10000] // 7 滴
+// const arr = new Array(1000000).fill(0).map(item => parseInt(Math.random() * 10)) // n 滴
+
+console.log('getRains3 收集雨滴数：', getRain3(arr))
+console.log('getRains2 收集雨滴数：', getRain2(arr))
+console.log('getRains 收集雨滴数：', getRain(arr))
+
+// 总结：
+//  getRain2 和 getRain 是一样的思路，只是代码 getRain2 更清爽一些
+//  getRain3 优化掉 操作 arr 这部分时间复杂度 的环节
