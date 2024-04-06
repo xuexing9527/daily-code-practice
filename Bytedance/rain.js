f9d50e7282e5252085022c720cdc716b3f22858f
fn <xuexing9527@163.com>
2024-04-06 22:44:57 +0800

    fix: getRains3, start and end

--- Bytedance/rain.js
+++ Bytedance/rain.js
@@ -30,7 +30,7 @@
 function getRain(arr) {
     const time = new Date().getTime()
     const column = arr.length
-    const row = arr.slice().sort()[column - 1]
+    const row = arr.slice().sort((a, b) => (a - b))[column - 1]
     let rain = 0
 
     const handleArr = (arr) => {
@@ -52,8 +52,11 @@ function getRain(arr) {
         })
 
         obj.lastIndex = arr.length - r
-        const payloadArr = arr.slice(obj.firstIndex, obj.lastIndex)
-        return payloadArr
+        if (obj.firstIndex !== undefined && obj.lastIndex !== undefined) {
+            const payloadArr = arr.slice(obj.firstIndex, obj.lastIndex)
+            return payloadArr
+        }
+        return []
     }
 
     // 每一次进入循环，数组要经过 去首尾 0， - i，再去 找首尾 0
@@ -89,7 +92,7 @@ function getRain(arr) {
 
 function getRain2(arr) {
     const time = new Date().getTime()
-    const lines = arr.slice().sort()[arr.length - 1]
+    const lines = arr.slice().sort((a, b) => (a - b))[arr.length - 1]
     const len = arr.length
     let rains = 0
 
@@ -101,7 +104,7 @@ function getRain2(arr) {
             if (arr[n] - i >= 1 && obj.lastItem === undefined) obj.lastItem = n
         }
         // 同时存在
-        if (obj.firstItem && obj.lastItem) {
+        if (obj.firstItem !== undefined && obj.lastItem !== undefined) {
             // Remember item - 1
             arr.slice(obj.firstItem, obj.lastItem + 1).map(item => item - i)
                 // 开始积累雨滴
@@ -127,50 +130,35 @@ function getRain3(arr) {
     const levelStartAndEnd = [
         { firstIndex: 0, lastIndex: arr.length } // 第一层 默认从头到尾遍历
     ]
-    let i = 0
+    let n = 0
     let rains = 0
     while (true) {
-        let levelRains = 0
         const obj = { firstIndex: undefined, lastIndex: undefined }
-        const preLevelObj = levelStartAndEnd[i]
+        const preLevelObj = levelStartAndEnd[n]
         if (preLevelObj) {
-            // 这里注意，奇数长度数组中间值 不会被遍历到。放到后面 m, n 存在的情况下，再判断中间值是否积累雨滴
-            for (let m = preLevelObj.firstIndex, n = preLevelObj.lastIndex; m < n; m += 1, n -= 1) {
-                if (arr[m] - i >= 1 && obj.firstIndex === undefined) { obj.firstIndex = m } // 从 m 位置开始积累雨滴
-                if (obj.firstIndex !== undefined && (m > obj.firstIndex && arr[m] - i <= 0)) { levelRains += 1 } // obj.firstIndex 存在时，arr[m] - i 小于等于 0 且 大于 m 位置时 积累雨滴
-                if (arr[n] - i >= 1 && obj.lastIndex === undefined) { obj.lastIndex = n } // 到 n 位置 结束积累雨滴，循环结束后，考虑 n 是否真的存在，如果不存在，当前层没有雨滴可接
-                if (obj.lastIndex !== undefined && (n < obj.lastIndex && arr[n] - i <= 0)) { levelRains += 1 } // obj.firstIndex 存在时，arr[m] - i 小于等于 0 且 大于 m 位置时 积累雨滴
-            }
+            for (let i = 0; i < preLevelObj.lastIndex; i += 1) { if (arr[i] - n >= 1) { obj.firstIndex = i; break; }}  // 从 i 位置开始积累雨滴
+            for (let j = preLevelObj.lastIndex; j > 0; j -= 1) { if (arr[j] - n >= 1) { obj.lastIndex = j; break; }} // 到 j 位置 结束积累雨滴
 
-            // 如果 m, n 相遇，没有合适的 m 或 n，舍弃当前层的雨滴。只有当 m, n 都存在时，levelRains 才成立
-            if (obj.firstIndex !== undefined && obj.lastIndex !== undefined) {
-                levelStartAndEnd.push(obj) // 存入层级 obj，减少下次遍历的长度
-                const len = preLevelObj.lastIndex + 1 - obj.firstIndex
-                // 如果数组长度是奇数，判断中间节点是否满足接雨滴的条件
-                if (len % 2 !== 0) {
-                    const middleIndex = (preLevelObj.firstIndex + preLevelObj.lastIndex) / 2
-                    if (arr[middleIndex] - i <= 0) rains += 1
-                }
-                rains = rains + levelRains // 积累当前层雨滴
+            // 积累雨滴
+            if (obj.firstIndex < obj.lastIndex) {
+                for (let i = obj.firstIndex; i < obj.lastIndex; i += 1) { if (arr[i] - n <= 0) rains += 1 }
+                levelStartAndEnd.push(obj)
             }
         } else {
-            // 上一次都没积累雨滴，直接break
-            break;
+            break // 上一次都没积累雨滴，直接break
         }
-        i += 1
+        n += 1
     }
     console.log('getRain3耗时：', new Date().getTime() - time)
     return rains
 }
 
-
-
 // const arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1] // 6 滴
 // const arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 0, 1] // 6 滴
 // const arr = [0, 1, 0, 2, 1, 0, 1, 0, 2, 1, 2, 1] // 8 滴
 // const arr = [0, 1, 0, 2, 1, 0, 1, 3, 0, 1, 2, 1] // 8 滴
-const arr = [10000 ,0 ,999 , 10000] // 7 滴
-// const arr = new Array(1000000).fill(0).map(item => parseInt(Math.random() * 10)) // n 滴
+// const arr = [10000 ,0 ,999 , 10000] // 19001 滴
+const arr = new Array(1000000).fill(0).map(_ => parseInt(Math.random() * 10)) // n 滴
 
 console.log('getRains3 收集雨滴数：', getRain3(arr))
 console.log('getRains2 收集雨滴数：', getRain2(arr))
