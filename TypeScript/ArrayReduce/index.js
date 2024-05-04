235b05db6dacac1758fd5c1d1cf1bc7522dea60f
xue xing <xuexing9527@163.com>
2024-05-04 13:06:33 +0800

    refactor: notes =>

--- TypeScript/ArrayReduce/index.js
+++ TypeScript/ArrayReduce/index.js
@@ -1,4 +1,5 @@
 var arr = [1, 2, [3, 4, [5, 6, 7, [8, [14, 15, 16], 9]]], 10, 11, [12, 13]];
+// const arrFlatten = (arr: any) => arr.reduce((accumulator: any, cur: any, curIndex: number, arr: any) => accumulator.concat(Array.isArray(cur) ? arrFlatten(cur) : [cur]), [])
 function arrFlatten(arr) {
     return arr.reduce(function (accumulator, cur, curIndex, arr) {
         return accumulator.concat(Array.isArray(cur) ? arrFlatten(cur) : [cur]);
--- TypeScript/ArrayReduce/index.ts
+++ TypeScript/ArrayReduce/index.ts
@@ -1,8 +1,10 @@
 const arr = [1, 2, [3, 4, [5, 6, 7, [8, [ 14, 15, 16] , 9]]], 10, 11, [12, 13]]
 
+// const arrFlatten = (arr: any) => arr.reduce((accumulator: any, cur: any, curIndex: number, arr: any) => accumulator.concat(Array.isArray(cur) ? arrFlatten(cur) : [cur]), [])
 function arrFlatten (arr: any) {
     return arr.reduce((accumulator: any, cur: any, curIndex: number, arr: any) => {
         return accumulator.concat(Array.isArray(cur) ? arrFlatten(cur) : [cur])
     }, [])
 }
+
 console.log(arrFlatten(arr))
