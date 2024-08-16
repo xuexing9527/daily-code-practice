20d211c59203287c841bf69a0310dd868b66e27d
xue xing <xuexing9527@163.com>
2024-08-16 22:27:29 +0800

    done

--- test3.js
+++ test3.js
@@ -12,27 +12,27 @@ function flattObj (obj) {
     const target = {};
     // const 
     // 深层遍历，拼接 key
-    const recurson = (obj, preKey, targetObj) => {
-        console.log(targetObj)
+    const recurson = (obj, preKey) => {
         // 遍历对象
         for (const key in obj) {
-            // 类型，对象，数组，
-            // 数组
-            if (Array.isArray(obj[key])) {
-                targetObj[`${preKey}.${key}`] = []
-                recurson(obj[key], key, targetObj[`${preKey}.${key}`])
-            } else if (Object.prototype.toString.call(obj[key]) === '[object Object]') { // Obj 类型
-                // 拼 key ，点 的判断
-                targetObj[`${preKey}.${key}`] = {}
-                recurson(obj[key], key, targetObj[`${preKey}.${key}`])
-            } else {
-                console.log(targetObj[`${preKey}.${key}`])
-                targetObj[`${preKey}.${key}`] = obj[key]
+            if (obj.hasOwnProperty(key)) {
+                const pKey = `${preKey ? `${preKey}.` : ''}${key}`
+                // 类型，对象，数组，
+                // 数组
+                if (Array.isArray(obj[key])) {
+                    recurson(obj[key], pKey)
+                } else if (Object.prototype.toString.call(obj[key]) === '[object Object]') { // Obj 类型
+                    // 拼 key ，点 的判断
+                    recurson(obj[key], pKey)
+                } else {
+                    console.log(pKey)
+                    target[pKey] = obj[key]
+                }
             }
         }
     }
 
-    recurson(obj, '', target);
+    recurson(obj, '');
     console.log(target)
 }
 
