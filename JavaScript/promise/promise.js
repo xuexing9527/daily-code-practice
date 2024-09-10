e569523fec255d9aa16f6a66c44696136928baf8
fn <xuexing9527@163.com>
2024-09-10 23:46:58 +0800

    learn  promise

--- JavaScript/promise/promise.js
+++ JavaScript/promise/promise.js
@@ -1,4 +1,6 @@
 new Promise(function() { console.log('为什么这里会直接执行？这不该是个回调吗？'); })
 new Date(() => {console.log('这里会执行吗？')})
 
-// 这里说明 promise 接收的回调函数 会 在 new 的过程中执行一次，这是为什么呢？
+// 这里说明 promise 接收的回调函数会在 new 的过程中执行一次，这是为什么呢？
+
+Promise
